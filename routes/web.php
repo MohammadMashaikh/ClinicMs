86bb43ac46d9b301eb98bdd706af253137cc7e43
Mohammad Mashaikh <mohammadmashaikh99@yahoo.com>
2026-01-23 17:11:56 +0300

    delete verified middleware

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(['auth'])->group(function () {
 
     Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
